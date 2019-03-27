9ec6fd934b023d5b0f35501e921007977d686f05
Binjie <luobinjie2017@gmail.com>
2019-03-26 22:12:03 -0400

    Finish users/session action and reducer

---
+++ frontend/action/session_actions.js
@@ -0,0 +1,39 @@
+import * as SessionApiUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+export const receiveCurrentUser = user => ({
+    type: RECEIVE_CURRENT_USER,
+    user
+});
+
+export const logoutCurrentUser = () => ({
+    type: LOGOUT_CURRENT_USER,
+});
+
+export const receiveErrors = (errors) => ({
+    type: RECEIVE_ERRORS,
+    errors
+});
+
+export const login = (user) => {
+    return SessionApiUtil.login(user).then(
+        (user) => dispatch(receiveCurrentUser(user)),
+        (err) => dispatch(receiveErrorss(err.responseJSON))
+    );
+};
+
+export const signup = (user) => {
+    return SessionApiUtil.signup(user).then(
+        (user) => dispatch(receiveCurrentUser(user)),
+        (err) => dispatch(receiveErrorss(err.responseJSON))
+    );
+};
+
+export const logout = () => {
+    return SessionApiUtil.logout().then(
+        user => dispatch(logoutCurrentUser())
+    );
+};
---
+++ frontend/reducer/users_reducer.js
@@ -0,0 +1,21 @@
+import {
+    RECEIVE_CURRENT_USER,
+} from '../action/session_actions';
+import merge from 'lodash/merge';
+
+const _defaultState = {
+    
+};
+
+const usersReducer = (state = _defaultState, action) => {
+    Object.freeze(state);
+    switch(action) {
+        case RECEIVE_CURRENT_USER: {
+            return merge({}, state, {[action.user.id]: action.user});
+        }
+        default:
+            return state;
+    }
+};
+
+export default usersReducer;
